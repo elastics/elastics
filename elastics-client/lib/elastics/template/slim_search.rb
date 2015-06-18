e9a2595bf00a8f4573170a0bf5ef402459ee5fb7
Domizio Demichelis <dd.nexus@gmail.com>
2015-06-18 08:54:30 +0700

    slim search restriction uses :_source instead of :fields

--- elastics-client/lib/elastics/template/slim_search.rb
+++ elastics-client/lib/elastics/template/slim_search.rb
@@ -2,10 +2,10 @@ module Elastics
   class Template
     class SlimSearch < Search
 
-      # removes the fields param (no _source returned)
+      # no _source returned
       # the result.loaded_collection, will load the records from the db
       def self.variables
-        super.deep_merge!(:params => {:fields => ''})
+        super.deep_merge!(:params => {:_source => false})
       end
 
     end
