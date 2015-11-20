79d6c71b5202974a27efa548ee38d70ab0f88687
Domizio Demichelis <dd.nexus@gmail.com>
2015-11-20 18:19:49 +0700

    removed experimental (and not working) method missing from elastics proxy

--- elastics-client/lib/elastics/class_proxy/templates/search.rb
+++ elastics-client/lib/elastics/class_proxy/templates/search.rb
@@ -37,21 +37,18 @@ module Elastics
         # implements search_type=scan (https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-search-type.html#scan)
         def scan_search(template, *vars, &block)
           user_raw_result = vars.any?{|v| v[:raw_result]}
-          scroll      = '5m'
-          search_vars = Vars.new({:params     => { :search_type => 'scan',
-                                                   :scroll      => scroll,
-                                                   :size        => 50 },
-                                  :raw_result => true}, *vars)
-          search_temp = template.is_a?(Elastics::Template) ? template : templates[template]
+          scroll          = '5m'
+          search_vars     = Vars.new({:params     => { :search_type => 'scan',
+                                                       :scroll      => scroll,
+                                                       :size        => 50 },
+                                      :raw_result => true}, *vars)
+          search_temp     = template.is_a?(Elastics::Template) ? template : templates[template]
 
-          scroll_vars = Vars.new({:params     => { :scroll => scroll },
-                                  :raw_result => true}, variables, *vars)
-          scroll_temp = Elastics::Template.new( :get,
-                                            '/_search/scroll',
-                                            nil,
-                                            scroll_vars )
-          search_res  = search_temp.render search_vars
-          scroll_id   = search_res['_scroll_id']
+          scroll_vars     = Vars.new({:params     => { :scroll => scroll },
+                                      :raw_result => true}, variables, *vars)
+          scroll_temp     = Elastics::Template.new( :get, '/_search/scroll', nil, scroll_vars )
+          search_res      = search_temp.render search_vars
+          scroll_id       = search_res['_scroll_id']
           while (result = scroll_temp.render(:data => scroll_id)) do
             break if result['hits']['hits'].empty?
             scroll_id = result['_scroll_id']
@@ -64,38 +61,6 @@ module Elastics
         def count_search(template, *vars)
           template = template.is_a?(Elastics::Template) ? template : templates[template]
           template.render Vars.new({:params => {:search_type => 'count'}, :raw_result => true}, *vars)
-        end
-
-
-        def method_missing(meth, *vars, &block)
-          meth.to_s =~ /(\w+)_(exists?\??|valid\??|validate|count|explain)$/
-          template_method = $1 && $1.to_sym
-          return super unless template_method && respond_to?(template_method)
-
-          render = lambda {|path| templates[template_method].render(Vars.new({:path => path, :raw_result => true}, *vars))}
-
-          case meth.to_s
-
-          # implements https://www.elastic.co/guide/en/elasticsearch/reference/current/search-exists.html
-          when /exists?\??$/
-            result = render.call '/<<index>>/<<type>>/<<id= ~ >>/_search/exists'
-            result['exists']
-
-          # implements https://www.elastic.co/guide/en/elasticsearch/reference/current/search-validate.html
-          when /(_valid\??|validate)$/
-            result = render.call '/<<index>>/<<type>>/<<id= ~ >>/_validate/query'
-            result['valid']
-
-          # implements the count API (https://www.elastic.co/guide/en/elasticsearch/reference/current/search-count.html)
-          when /_count$/
-            result = render.call '/<<index>>/<<type>>/_count'
-            result['count']
-
-          # implements https://www.elastic.co/guide/en/elasticsearch/reference/current/search-explain.html
-          when /_explain$/
-            render.call '/<<index>>/<<type>>/<<id= ~ >>/_explain'
-
-          end
         end
 
       end
