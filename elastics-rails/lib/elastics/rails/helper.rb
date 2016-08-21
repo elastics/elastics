68a13116f4775e8c1a019b432c0326eca0846b34
Domizio Demichelis <dd.nexus@gmail.com>
2016-08-21 17:37:17 +0700

    no need to load the models

--- elastics-rails/lib/elastics/rails/helper.rb
+++ elastics-rails/lib/elastics/rails/helper.rb
@@ -6,7 +6,7 @@ module Elastics
       def after_initialize
         # we need to reload the elastics API methods with the new variables
         Elastics.reload!
-        Conf.elastics_models && Conf.elastics_models.each {|m| eval"::#{m}" if m.is_a?(String) }
+        #Conf.elastics_models && Conf.elastics_models.each {|m| eval"::#{m}" if m.is_a?(String) }
       end
 
     end
