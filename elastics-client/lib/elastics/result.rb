e6e4d319396c58d5fd1bbb71dbf1d50b4377bb24
Domizio Demichelis <dd.nexus@gmail.com>
2016-10-01 15:21:17 +0200

    logging of :raw_result if it is a defined method of the custom result class (avoid to return custom structures into the elastics log)

--- elastics-client/lib/elastics/result.rb
+++ elastics-client/lib/elastics/result.rb
@@ -4,11 +4,11 @@ module Elastics
     attr_reader :template, :response
     attr_accessor :variables
 
-    def initialize(template, variables, response, result=nil)
+    def initialize(template, variables, response, raw_result=nil)
       @template  = template
       @variables = variables
       @response  = response
-      replace result || !response.body.empty? && MultiJson.decode(response.body) || return
+      replace raw_result || !response.body.empty? && MultiJson.decode(response.body) || return
       Conf.result_extenders.each do |ext|
         next if ext.respond_to?(:should_extend?) && !ext.should_extend?(self)
         extend ext
